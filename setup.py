8c61bdd68f81c6a98ddf6af03af93819994a264d
Alex Weiss <algrs@cacography.net>
2015-07-06 11:01:58 -0400

    Remove monkey patch to setuptools that appears to no longer be necessary

--- setup.py
+++ setup.py
@@ -18,14 +18,6 @@ except:
     cythonize = lambda x : x
     have_cython = False
 
-
-# setuptools DWIM monkey-patch madness
-# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-import sys
-if 'setuptools.extension' in sys.modules:
-    m = sys.modules['setuptools.extension']
-    m.Extension.__dict__ = m._Extension.__dict__
-
 context_dir = os.path.join(os.path.dirname(__file__), 'contexts')
 
 osmesa_mirrors = [
